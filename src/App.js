4e05af25d8621c371ea9fd1b9599bb7ccc319393
patrickswalls@gmail.com <patrickswalls@gmail.com>
2018-01-18 23:02:11 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -6,13 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Lil Croix</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/91ZFRxGaxYL._SY679_.jpg"/>
+        <h1>Coming Soon</h1>
       </div>
     );
   }
